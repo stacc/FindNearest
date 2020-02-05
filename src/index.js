c01930e9589b2d1ded935248dbe2dc6d9c46f275
Kristoffer-Andre Kalliainen <Kristoffer-Andre.Kalliainen@stacc.com>
2020-02-05 16:18:43 +0100

    Clean up

--- src/index.js
+++ src/index.js
@@ -11,14 +11,13 @@ const positionData = [
     "latitude": 60.3963703,
     "longitude": 5.3242669,
     "cleanliness": 1
- },
- {
-  "id": 1,
-  "latitude": 60.3879681,
-  "longitude": 5.334608,
-  "cleanliness": 1
-},
-
+  },
+  {
+    "id": 1,
+    "latitude": 60.3879681,
+    "longitude": 5.334608,
+    "cleanliness": 1
+  },
 ]
 
 /* Second assignment */
@@ -51,53 +50,52 @@ const ratingoverlay = () => new ScatterplotLayer({
   getFillColor: d => d.cleanliness === 0 ? [200, 0, 40, 150] : d.cleanliness === 1 ? [138, 255, 119] : [255, 140, 0, 100],
   pickable: true,
   onHover: ({object, x, y}) => {
-      const el = document.getElementById('tooltip');
-      if (object) {
-        const { id, adresse, cleanliness } = object;
-        el.innerHTML = `<h3>${adresse}</h3>
-        <section>
-          <p>${cleanliness === 0 ? 'Styr unna med mindre du virkelig må på do!' : cleanliness === 2 ? 'heilt ok' : 'Her kan du trygt gå!' }</p>
-        </section>`;
-        el.style.display = 'block';
-        el.style.opacity = 0.9;
-        el.style.left = x + 'px';
-        el.style.top = y + 'px';
-      } else {
-        el.style.opacity = 0.0;
-      }
+    const el = document.getElementById('tooltip');
+    if (object) {
+      const { id, adresse, cleanliness } = object;
+      el.innerHTML = `<h3>${adresse}</h3>
+      <section>
+        <p>${cleanliness === 0 ? 'Styr unna med mindre du virkelig må på do!' : cleanliness === 2 ? 'heilt ok' : 'Her kan du trygt gå!' }</p>
+      </section>`;
+      el.style.display = 'block';
+      el.style.opacity = 0.9;
+      el.style.left = x + 'px';
+      el.style.top = y + 'px';
+    } else {
+      el.style.opacity = 0.0;
+    }
   }
 });
 
-
 const scatterplot = () => new ScatterplotLayer({
-    id: 'toiletView',
-    data: sourceData,
-    opacity: 0.8,
-    filled: true,
-    radiusMinPixels: 21,
-    radiusMaxPixels: 26,
-    getPosition: d => [d.longitude, d.latitude],
-    getFillColor: d => d.pris > 10 ? [200, 0, 40, 150] : d.pris == 'NULL' ? [138, 255, 119] : [255, 140, 0, 100],
-    pickable: true,
-    onHover: ({object, x, y}) => {
-        const el = document.getElementById('tooltip');
-        if (object) {
-          const { id, adresse, pris, tid_hverdag, tid_lordag, tid_sondag } = object;
-          el.innerHTML = `<h3>${adresse}</h3>
-          <ul>
-          <li>Pris: ${pris == 'NULL' ? 'Gratis' : pris}</li>
-          <li>Åpningstid kvardag: ${tid_hverdag == 'ALL' && tid_hverdag ? 'Døgnåpent' : tid_hverdag}</li>
-          <li>Åpningstid laurdag: ${tid_lordag == 'NULL' && tid_lordag ? 'Stengt' : tid_lordag}</li>
-          <li>Åpningstid sundag: ${tid_sondag  == 'NULL' && tid_sondag ? 'Stengt' : tid_sondag}</li>
-          </ul>`;
-          el.style.display = 'block';
-          el.style.opacity = 0.9;
-          el.style.left = x + 'px';
-          el.style.top = y + 'px';
-        } else {
-          el.style.opacity = 0.0;
-        }
+  id: 'toiletView',
+  data: sourceData,
+  opacity: 0.8,
+  filled: true,
+  radiusMinPixels: 21,
+  radiusMaxPixels: 26,
+  getPosition: d => [d.longitude, d.latitude],
+  getFillColor: d => d.pris > 10 ? [200, 0, 40, 150] : d.pris == 'NULL' ? [138, 255, 119] : [255, 140, 0, 100],
+  pickable: true,
+  onHover: ({object, x, y}) => {
+    const el = document.getElementById('tooltip');
+    if (object) {
+      const { id, adresse, pris, tid_hverdag, tid_lordag, tid_sondag } = object;
+      el.innerHTML = `<h3>${adresse}</h3>
+      <ul>
+        <li>Pris: ${pris == 'NULL' ? 'Gratis' : pris}</li>
+        <li>Åpningstid kvardag: ${tid_hverdag == 'ALL' && tid_hverdag ? 'Døgnåpent' : tid_hverdag}</li>
+        <li>Åpningstid laurdag: ${tid_lordag == 'NULL' && tid_lordag ? 'Stengt' : tid_lordag}</li>
+        <li>Åpningstid sundag: ${tid_sondag  == 'NULL' && tid_sondag ? 'Stengt' : tid_sondag}</li>
+      </ul>`;
+      el.style.display = 'block';
+      el.style.opacity = 0.9;
+      el.style.left = x + 'px';
+      el.style.top = y + 'px';
+    } else {
+      el.style.opacity = 0.0;
     }
+  }
 });
 
 const hexagon = () => new HexagonLayer({
@@ -126,61 +124,60 @@ const selectedView = () => {
   }
 }
 
-
 window.initMap = () => {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 60.39, lng: 5.32 },
-        zoom: 15,
-        styles: styles
-    });
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 60.39, lng: 5.32 },
+    zoom: 15,
+    styles: styles
+  });
 
-    const overlay = new GoogleMapsOverlay({
+  const overlay = new GoogleMapsOverlay({
+    layers: [
+      scatterplot()
+    ],
+  });
+
+  selectedView();
+  overlay.setMap(map);
+
+  window.setPriceOverview = () => {
+    overlay.setProps({
       layers: [
-        scatterplot()
+          scatterplot()
       ],
     });
+  }
 
-    selectedView();
-    overlay.setMap(map);
+  window.setHeatMap = () => {
+    overlay.setProps({
+      layers: [
+        hexagon(),
+        scatterplot()
+      ]
+    })
+  }
 
-    window.setPriceOverview = () => {
-      overlay.setProps({
-        layers: [
-            scatterplot()
-        ],
-      });
-    }
+  /* First assignment: add a filter based on best rating */
+  window.setCleanlinessRating = () => {
+    overlay.setProps({
+      layers: [
+        ratingoverlay()
+      ]
+    })
+  }
 
-    window.setHeatMap = () => {
-      overlay.setProps({
-        layers: [
-          hexagon(),
-          scatterplot()
-        ]
-      })
-    }
+  /* Second assignment */
+  window.emergencyNow = () => {
+    overlay.setProps({
+      layers: [
+        emergencyNow()
+      ]
+    })
+  }
 
-    /* First assignment: add a filter based on best rating */
-    window.setCleanlinessRating = () => {
-      overlay.setProps({
-        layers: [
-          ratingoverlay()
-        ]
-      })
-    }
-
-    /* Second assignment */
-    window.emergencyNow = () => {
-      overlay.setProps({
-        layers: [
-          emergencyNow()
-        ]
-      })
-    }
-
-    window.resetViews = () => {
-      overlay.setProps({
-        layers: []
-      })
-    }
+  window.resetViews = () => {
+    overlay.setProps({
+      layers: []
+    })
+  }
 }
